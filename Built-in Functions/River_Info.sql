fca708d7e7effd87fe95cb46feeddebf4ea3de2a
Georgy <graykov7@gmail.com>
2024-12-10 16:44:13 +0200

    Create a view named "view_river_info" that concatenates the "river_name", "outflow" and "length" columns from the "rivers" table

---
+++ Built-in Functions/River_Info.sql
@@ -0,0 +1,11 @@
+SELECT
+   CONCAT_WS(' ',
+   'The river', 
+   river_name, 
+   'flows into the', 
+   outflow, 
+   'and is', 
+   "length", 
+   'kilometers long.') AS "River Information"
+FROM rivers
+ORDER BY river_name ASC;
